2def0ba7d29a3918f2ef90f425eae6b5a4ec63b1
Aaron Jen <aaronjen@jubo.health>
2020-12-25 14:09:11 +0800

    commit maximum-subarray

---
+++ 53.maximum-subarray/main.go
@@ -0,0 +1,28 @@
+package main
+
+/*
+ * @lc app=leetcode id=53 lang=golang
+ *
+ * [53] Maximum Subarray
+ */
+
+// @lc code=start
+func maxSubArray(nums []int) int {
+	res := nums[0]
+	cur := nums[0]
+	for i := 1; i < len(nums); i++ {
+		if cur > 0 {
+			cur += nums[i]
+		} else {
+			cur = nums[i]
+		}
+
+		if cur > res {
+			res = cur
+		}
+	}
+
+	return res
+}
+
+// @lc code=end
