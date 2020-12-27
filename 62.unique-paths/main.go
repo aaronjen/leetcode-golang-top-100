96737e64d9e9a3c26d6e1a5d3dfc1bcb35a75c1c
Aaron Jen <aaronjen@jubo.health>
2020-12-27 15:13:00 +0800

    commit unique-paths

---
+++ 62.unique-paths/main.go
@@ -0,0 +1,25 @@
+package main
+
+/*
+ * @lc app=leetcode id=62 lang=golang
+ *
+ * [62] Unique Paths
+ */
+
+// @lc code=start
+func uniquePaths(m int, n int) int {
+	dp := make([]int, n)
+	for i := 0; i < n; i++ {
+		dp[i] = 1
+	}
+
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			dp[j] += dp[j-1]
+		}
+	}
+
+	return dp[n-1]
+}
+
+// @lc code=end
